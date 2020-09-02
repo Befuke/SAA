716aff9a02a271ce353e65d17852290ef3c3a036
Befuke <befuke.alexis@gmail.com>
2020-09-02 21:02:27 +0200

    Array changed to Arraylist in the Seat Class

--- src/main/java/com/flysaa/seat/Seat.java
+++ src/main/java/com/flysaa/seat/Seat.java
@@ -1,37 +1,24 @@
 package com.flysaa.seat;
-import java.util.Scanner;
+import java.util.*;
 public class Seat{
-static int seatNo[] = new int[25];
+public ArrayList<Integer> seat = new ArrayList<>();
 private int seatNumber;
 Scanner scan = new Scanner(System.in);
 public Seat(){
 int i = 1;
 do{
 System.out.println("Enter Seat Number");
-seatNo[i] = scan.nextInt();
-if(seatNo[i] == seatNo[i-1]){
+seatNumber = scan.nextInt();
+if(seatNumber == 0){
 System.out.println("Seat Taken");
+}else{
+seat.add(seatNumber);
 }
-
-
 }while(i<1);
 i++;
 }
 
-
-
-//public Seat(){
-//for( int i = 1; i<=26; i++){
-//if(i == 1){
-//System.out.println("Enter Seat Number");
-//seatNo[0] = scan.nextInt();
-//if(seatNo[0] < 1){
-//System.out.println("Seat Taken");
-//}
-//}
-//}
-//}
 public int getSeatNo(){
-return seatNo[0];
+return seatNumber;
 }
 }
